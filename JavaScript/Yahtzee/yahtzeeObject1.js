73cdf15bc93f9cc1e3d85e4c7c6e9ef59a296c26
zbaker5 <zachary.baker057@gmail.com>
2018-02-19 09:44:55 -0600

    Yahtzee Beginning

---
+++ JavaScript/Yahtzee/yahtzeeObject1.js
@@ -0,0 +1,97 @@
+yahtzee = {
+  'turnsRemaining' : 13,
+   'throwsRemainingInTurn' : 3,
+   'player' : {
+     'name' : 'Zach',
+     'avatar': 'https://www.google.com'
+   }
+  'dice' : [
+    {
+      'sideUp' : 1,
+      'saved' : true,
+    }
+    {
+      'sideUp' : 2,
+      'saved' : false,
+    }
+    {
+      'sideUp' : 3,
+      'saved' : true,
+    }
+    {
+      'sideUp' : 4,
+      'saved' : false,
+    }
+    {
+      'sideUp' : 5,
+      'saved' : false
+    }
+  ]
+  'scoreCard' : [
+    {
+      'title' : 'Ones',
+      'top' : true,
+      'displaySequence' : 1,
+      'scoreRecorded' : false,
+      'score' : 0,
+      'scoreCondition' : 'notSureYet',
+      'scoreMath' : 'sumOfOnes',
+    }
+    {
+      'title' : 'Twos',
+      'top' : true,
+      'displaySequence' : 2,
+      'scoreRecorded' : false,
+      'score' : 0,
+      'scoreCondition' : 'notSureYet',
+      'scoreMath' : 'sumOfTwos',
+    }
+    {
+      'title' : 'Threes',
+      'top' : true,
+      'displaySequence' : 3,
+      'scoreRecorded' : false,
+      'score' : 0,
+      'scoreCondition' : 'notSureYet',
+      'scoreMath' : 'sumOfThrees',
+    }
+    {
+      'title' : 'Fours',
+      'top' : true,
+      'displaySequence' : 4,
+      'scoreRecorded' : false,
+      'score' : 0,
+      'scoreCondition' : 'notSureYet',
+      'scoreMath' : 'sumOfFours',
+    }
+    {
+      'title' : 'Fives',
+      'top' : true,
+      'displaySequence' : 5,
+      'scoreRecorded' : false,
+      'score' : 0,
+      'scoreCondition' : 'notSureYet',
+      'scoreMath' : 'sumOfFives',
+    }
+    {
+      'title' : 'Sixes',
+      'top' : true,
+      'displaySequence' : 6,
+      'scoreRecorded' : false,
+      'score' : 0,
+      'scoreCondition' : 'notSureYet',
+      'scoreMath' : 'sumOfOnes',
+    }
+    {
+      'title' : 'Full House',
+      'top' : false,
+      'displaySequence' : 9,
+      'scoreRecorded' : true,
+      'score' : 25,
+      'scoreCondition' : 'notSureYet',
+      'scoreMath' : 'fixedScoreOf25',
+    }
+
+  ]
+  }
+}
